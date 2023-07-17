19968b2bc62069f02755059c7c67f49c1790be40
devSelmaJr <60160024+selmargoulart08@users.noreply.github.com>
2023-07-17 04:00:12 -0300

    Update script.js

--- script.js
+++ script.js
@@ -10,6 +10,7 @@ function colocarNaTela(dados){
     document.querySelector(".temp").innerHTML =  Math.floor(dados.main.temp) + "°C"
     document.querySelector(".descricao").innerHTML = dados.weather[0].description
     document.querySelector(".icone").src = "https://openweathermap.org/img/wn/" + dados.weather[0].icon + ".png"
+    document.querySelector(".umidade").innerHTML = "Umidade " + dados.main.humidity + '%'
 }
 
 async function buscarCidade(cidade){
